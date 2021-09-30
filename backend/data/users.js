e7e5c58d0629b3dd5938b3a5f3e52b514bfa7443
ckc2002 <chaitanyachandra2000@gmail.com>
2021-09-30 12:24:47 +0530

    first

---
+++ backend/data/users.js
@@ -0,0 +1,28 @@
+import bcrypt from "bcryptjs";
+
+const users = [
+  {
+    name: "Admin",
+    email: "admin@comfy.com",
+    password: bcrypt.hashSync("123456", 10),
+    isAdmin: true,
+  },
+  {
+    name: "Admin",
+    email: "admin2@comfy.com",
+    password: 123,
+    isAdmin: true,
+  },
+  {
+    name: "Gyasuddin",
+    email: "gyasuddin@comfy.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+  {
+    name: "Laksh Sir",
+    email: "lakshsir@comfy.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+];
+
+export default users;
